df26ee410a133400a730bcae26dac436d6df132f
Yassine Saddiki <yassine.saddiki@etu.unilim.fr>
2024-04-04 17:31:37 +0200

    ajout de dependance + packages

--- androidMeteoYS/build.gradle.kts
+++ androidMeteoYS/build.gradle.kts
@@ -66,6 +66,27 @@ dependencies {
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
+    //--------//
+
+
+
+    implementation ("com.github.bumptech.glide:compose:1.0.0-beta01")
+
+
+
+    implementation ("com.google.code.gson:gson:2.8.9")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.6.0")
+
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:2.7.7")
+    debugImplementation ("androidx.compose.ui:ui-tooling:2.7.7")
+
 
 
 
